d67a5d26623a34b07bba018a50e47f22e9a6f755
Jeremy Voros <jeremyvoros@gmail.com>
2018-06-25 08:23:27 -0600

    min height on content

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   render() {
     return (
         <HashRouter>
-          <div>
+          <div className="row--stacked">
             <Header />
             <main className="content container" role="main">
               <Switch>
