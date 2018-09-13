7e9b89c125cae709cc9e74f717076486727e1dbe
Jeremy Voros <jeremy@Jeremys-MacBook-Pro.local>
2018-09-13 09:07:40 -0600

    revert to AWS search endpoint

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export default {
     categories: 'https://aliemcards.netlify.com/categories.json',
     recent: 'https://aliemcards.netlify.com/recent.json',
     summaries: 'https://aliemcards.netlify.com/cardsummaries.json',
-    search: 'https://aliemcards.netlify.com/.netlify/functions/search?query='
+    search: 'https://mexv6u9ex2.execute-api.us-east-1.amazonaws.com/dev/search/'
   }
 }
